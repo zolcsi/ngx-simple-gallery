7b32b4a2652c9329ac74c5814aadeabbce005fe3
Zoltan Magyar <zolcsi.san@gmail.com>
2025-01-07 16:18:55 +0100

    streamline directive

--- projects/ngx-simple-gallery/src/lib/open-gallery/open-gallery.directive.ts
+++ projects/ngx-simple-gallery/src/lib/open-gallery/open-gallery.directive.ts
@@ -2,8 +2,8 @@ import { Directive, ElementRef, HostListener, inject, Input, input } from '@angu
 import { ShowcaseDialogComponent } from '../showcase-dialog/showcase-dialog.component';
 import { GalleryService } from '../core/service/gallery.service';
 import { Dialog } from '@angular/cdk/dialog';
-import { GalleryItem } from '../core/model/gallery-item';
 import { ModalConfig } from '../core/model/modal-config';
+import { GalleryItem } from '../core/model/gallery-item';
 
 @Directive({
   // eslint-disable-next-line @angular-eslint/directive-selector
@@ -15,24 +15,24 @@ export class OpenGalleryDirective {
   private readonly elementRef = inject(ElementRef);
   private readonly galleryService = inject(GalleryService);
 
-  public readonly openGallery = input(0);
+  public readonly openGallery = input([] as GalleryItem[]);
 
   constructor() {
     this.elementRef.nativeElement.style.cursor = 'pointer';
   }
 
-  @Input({ required: true })
-  set galleryItems(items: GalleryItem[]) {
-    this.galleryService.setGalleryItems(items);
+  @Input()
+  set startIndex(index: number) {
+    this.galleryService.setItemIndex(index);
   }
 
-  @Input({ required: false })
+  @Input()
   set modalConfig(modalConfig: ModalConfig) {
     this.galleryService.setModalConfig(modalConfig);
   }
 
   @HostListener('click') onClick(): void {
-    this.galleryService.setItemIndex(this.openGallery());
+    this.galleryService.setGalleryItems(this.openGallery());
     this.dialog.open(ShowcaseDialogComponent);
   }
 }
