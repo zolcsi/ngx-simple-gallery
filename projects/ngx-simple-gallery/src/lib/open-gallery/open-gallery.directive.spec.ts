d51cc663ad7ea8c90b306153829f4f8ad67e3dda
Zoltan Magyar <zolcsi.san@gmail.com>
2025-01-07 16:44:31 +0100

    added tests

--- projects/ngx-simple-gallery/src/lib/open-gallery/open-gallery.directive.spec.ts
+++ projects/ngx-simple-gallery/src/lib/open-gallery/open-gallery.directive.spec.ts
@@ -1,8 +1,75 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { OpenGalleryDirective } from './open-gallery.directive';
+import { GalleryService } from '../core/service/gallery.service';
+import { Dialog } from '@angular/cdk/dialog';
+import { signal } from '@angular/core';
 
 describe('OpenGalleryDirective', () => {
-  it('should create an instance', () => {
-    const directive = new OpenGalleryDirective();
-    expect(directive).toBeTruthy();
+  let fixture: ComponentFixture<OpenGalleryDirective>;
+  let directive: OpenGalleryDirective;
+  let galleryServiceMock: Partial<GalleryService>;
+
+  beforeEach(async () => {
+    galleryServiceMock = {
+      setItemIndex: jest.fn(),
+      getItemIndex: signal(0),
+      setModalConfig: jest.fn(),
+      setGalleryItems: jest.fn(),
+    };
+
+    await TestBed.configureTestingModule({
+      imports: [OpenGalleryDirective],
+      providers: [
+        { provide: Dialog, useValue: {} },
+        { provide: GalleryService, useValue: galleryServiceMock },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(OpenGalleryDirective);
+    directive = fixture.componentInstance;
+    fixture.detectChanges();
   });
+
+  it('should set the cursor style to pointer', () => {
+    const debugElement = fixture.debugElement;
+    const nativeElement = debugElement.nativeElement;
+    expect(nativeElement.style.cursor).toBe('pointer');
+  });
+
+  it('should set the start index of the gallery service', () => {
+    const startIndex = 2;
+    directive.startIndex = startIndex;
+    expect(galleryServiceMock.getItemIndex).toHaveBeenCalled();
+  });
+
+  // it('should set the modal config of the gallery service', () => {
+  //   const modalConfig: ModalConfig = {
+  //     width: '500px',
+  //     height: '500px',
+  //   };
+  //   directive.modalConfig = modalConfig;
+  //   expect(directive.galleryService.getModalConfig()).toEqual(modalConfig);
+  // });
+  //
+  // it('should set the gallery items of the gallery service', () => {
+  //   const galleryItems: GalleryItem[] = [
+  //     {
+  //       url: 'https://example.com/image1.jpg',
+  //       alt: 'Image 1',
+  //     },
+  //     {
+  //       url: 'https://example.com/image2.jpg',
+  //       alt: 'Image 2',
+  //     },
+  //   ];
+  //   directive.openGallery = galleryItems;
+  //   expect(directive.galleryService.getGalleryItems()).toEqual(galleryItems);
+  // });
+  //
+  // it('should open the showcase dialog when clicked', () => {
+  //   const dialog = TestBed.inject(Dialog);
+  //   spyOn(dialog, 'open');
+  //   directive.onClick();
+  //   expect(dialog.open).toHaveBeenCalledWith(ShowcaseDialogComponent);
+  // });
 });
--- projects/ngx-simple-gallery/src/lib/spinner/spinner.component.spec.ts
+++ projects/ngx-simple-gallery/src/lib/spinner/spinner.component.spec.ts
@@ -20,4 +20,19 @@ describe('SpinnerComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render the spinner container with the correct class', () => {
+    const containerElement = fixture.nativeElement.querySelector('.lds-ring');
+    expect(containerElement).toBeTruthy();
+  });
+
+  it('should render four child div elements', () => {
+    const childElements = fixture.nativeElement.querySelectorAll('.lds-ring div');
+    expect(childElements.length).toBe(4);
+  });
+
+  it('should not render any other elements besides the spinner container and its child divs', () => {
+    const allElements = fixture.nativeElement.querySelectorAll('*');
+    expect(allElements.length).toBe(5); // 1 container + 4 child divs
+  });
 });
